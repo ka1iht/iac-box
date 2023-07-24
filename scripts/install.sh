f9f0a5010511cb8301d8455d635d6a83bea6c020
ka1iht <thilak746@gmail.com>
2023-07-24 15:08:53 -0500

    Add working files

---
+++ scripts/install.sh
@@ -0,0 +1,9 @@
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt-get update && sudo apt-get install terraform
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt-get -y install zip
+unzip awscliv2.zip
+sudo ./aws/install
+terraform -version
+aws --version
