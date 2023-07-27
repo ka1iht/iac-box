9d3655124a65b0aed00f86bf423c02d4bcad2ec5
ka1iht <thilak746@gmail.com>
2023-07-27 13:47:52 -0500

    add more libraries

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,34 @@
+# install Terraform
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt-get update && sudo apt-get install terraform
+sudo apt-get update && sudo apt-get -y install terraform
+
+# install Packer
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get -y install packer
+
+# install aws-cli
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo apt-get -y install zip
 unzip awscliv2.zip
 sudo ./aws/install
-sudo apt-get -y upgrade
+
+# install Docker
+sudo apt-get -y install docker.io
+
+# install Ansible
+sudo apt-get -y install ansible
+
+# give Docker permissions
+usermod -G docker vagrant
+
+# clean and upgrade
+sudo apt-get -y clean && sudo apt-get -y upgrade
+
+# version checks
 terraform -version
+packer --version
 aws --version
+ansible --version
+docker --version
